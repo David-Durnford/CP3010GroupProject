2202d702339b5a0a52d00b016038e6dca4876979
dalecroft1989 <dalecroft1989@gmail.com>
2023-04-03 22:30:32 -0230

    Connected local mongoDB connection to server for testing Added a get route to download trivia questions to import to the server daily Added TODO's for other POST's/ GETs we might need

--- express_backend/src/server.js
+++ express_backend/src/server.js
@@ -1,10 +1,47 @@
 import express from 'express';
-const app = express();
-const port = 8000
+import * as dotenv from 'dotenv';
+import {MongoClient } from 'mongodb';
+dotenv.config();
 
-app.get('/', function(req, res){
-    res.send("Hello world!");
-});
+const app = express();
+const port = 8000;
+const dbName = 'trivia';
+app.get('/api/fetchQuestions', async (req, res) => {
+    fetch(process.env.QUESTIONSAPI)
+        .then(response => response.json())
+        .then(async data => {
+            const currDate = new Date();
+            for (const dataElement of data) {
+                dataElement['date'] = `${currDate.getFullYear()}-${currDate.getMonth() + 1}-${currDate.getDate()}`;
+            }
+            console.log(data)
+            const client = new MongoClient(process.env.MONGO_CONNECT);
+            await client.connect();
+            const db = client.db(dbName);
+            await db.collection('questions').insertMany(data);
+
+            res.send('Questions successfully fetched')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+app.post('/api/submitAnswers', async (req, res) => {
+  //TODO - Implement
+})
+
+app.post('/api/login', async (req, res) => {
+    //TODO - Implement
+})
+
+app.post('/api/register', async (req, res) => {
+    //TODO - Implement
+})
+
+app.get('/api/getScore', async (req, res) => {
+    //TODO - Implement
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
