eaa9d8b972238fe6c1a5a54d56ec349b3f8535c0
durnf <dreadlordfluffybunny@gmail.com>
2023-04-05 10:05:37 -0230

    added mongoose setup for our Users collection. and also created teh registerUsers route which checks if email exists in the db if not hashes the passed in password and creates a new entry.

--- express_backend/src/server.js
+++ express_backend/src/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import {MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import User from "./User.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 const port = 8000;
 const dbName = 'trivia';
+
 app.get('/api/fetchQuestions', async (req, res) => {
     fetch(process.env.QUESTIONSAPI)
         .then(response => response.json())
@@ -36,7 +42,30 @@ app.post('/api/login', async (req, res) => {
 })
 
 app.post('/api/register', async (req, res) => {
-    //TODO - Implement
+    const { name, email, password} = req.body
+    await mongoose.connect(process.env.MONGO_CONNECT+'/'+dbName)
+
+    try{
+        const findUser = await User.find({email: email})
+
+        if(findUser.length > 0){
+            res.send("User already exists")
+            return
+        }
+        let hashedPassword = await bcrypt.hash(password, 8)
+
+        const newUser = await User.create({
+            username: name,
+            email: email,
+            password: hashedPassword,
+            score: 0,
+            dateCreated: new Date()
+        })
+        res.send(newUser)
+    }catch (err){
+        res.send("User could not be created")
+        console.log(err)}
+
 })
 
 app.get('/api/getScore', async (req, res) => {
