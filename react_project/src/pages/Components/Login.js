e8aba9c7b9db8e63498831242c4804e0d12272b8
chapmandaniel <daniel.chapman@gmail.com>
2023-04-09 17:32:07 -0230

    Implemented user stats management and display

--- react_project/src/pages/Components/Login.js
+++ react_project/src/pages/Components/Login.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import {
-    MDBInput,
-    MDBCol,
-    MDBRow,
-    MDBCheckbox,
-    MDBBtn
-} from 'mdb-react-ui-kit';
+import React, { useState } from 'react';
+import { MDBInput, MDBBtn } from 'mdb-react-ui-kit';
 
-export default function App() {
+const Login = () => {
+
+
     return (
-        <form>
-            <MDBInput className='mb-4' type='email' id='form1Example1' label='Email address' />
-            <MDBInput className='mb-4' type='password' id='form1Example2' label='Password' />
-            <MDBBtn type='submit' block className='m-auto w-50 bg-warning'>
-                Sign in
-            </MDBBtn>
-        </form>
+        <div>
+            Hello World!
+        </div>
     );
-}
+};
+
+export default Login;
